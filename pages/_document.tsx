d6a5acc2f4db157cda817cc8732c091df7d47b44
francomd <francomdalmasso@gmail.com>
2021-06-05 21:28:33 -0300

    Add Google Tag Manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,30 @@ class MyDocument extends Document {
           <script
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
           ></script>
+          {/* <!-- Google Tag Manager --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PBMNWG9');`,
+            }}
+          />
+          {/* <!-- End Google Tag Manager --> */}
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PBMNWG9"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+              title="google-tag-manager"
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
